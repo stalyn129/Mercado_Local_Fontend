15e007eb9d74a2a35a433838c6452b18ea7a0a28
Stalyn2103 <stalynpesantez@gmail.com>
2025-11-24 10:06:21 -0500

    Update main.jsx

--- frond-ML/frond-ML/src/main.jsx
+++ frond-ML/frond-ML/src/main.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+  import React from "react";
+  import ReactDOM from "react-dom/client";
+  import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import App from "./App.jsx";
-import "./styles/global.css";
-import "./styles/colors.css";
-import "./styles/fonts.css";
+  import App from "./App.jsx";
+  import "./styles/global.css";
+  import "./styles/colors.css";
+  import "./styles/fonts.css";
 
-import Register from "./pages/Register";
-import Login from "./pages/LoginModal";
+  import Register from "./pages/Register";
+  import Login from "./pages/LoginModal";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/loginmodal" element={<Login />} />
-    </Routes>
-  </BrowserRouter>
-);
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/loginmodal" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
